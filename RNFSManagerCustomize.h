553221dc4bfbd52cddd545309c391aca182c67f7
KAz <kazhoang@gmail.com>
2023-08-16 11:57:18 +0700

    clone repo

---
+++ RNFSManagerCustomize.h
@@ -0,0 +1,19 @@
+//
+//  RNFSManagerCustomize.h
+//  RNFSManagerCustomize
+//
+//  Created by Johannes Lumpe on 08/05/15.
+//  Copyright (c) 2015 Johannes Lumpe. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTLog.h>
+
+typedef void (^CompletionHandler)(void);
+
+@interface RNFSManagerCustomize : RCTEventEmitter <RCTBridgeModule>
+
++(void)setCompletionHandlerForIdentifier: (NSString *)identifier completionHandler: (CompletionHandler)completionHandler;
+
+@end
